143b7c06bd17a862aeba96cc24b1ee458171e37b
izzamokhtar <92832461+izzamokhtar@users.noreply.github.com>
2024-12-17 17:57:06 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+#yang ni latest
+%%writefile app.py
+import streamlit as st
+import pandas as pd
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+from warnings import filterwarnings
+
+st.set_page_config(page_title="Rent Price Prediction", page_icon=":heavy_dollar_sign:")
+filterwarnings('ignore', category=FutureWarning)
+
+# Sidebar for navigation
+#page = st.sidebar.radio("Navigate", ["Rental Prediction", "Interactive Graphs", "Data Analysis"])
+# Sidebar navigation with icons
+page = st.sidebar.radio(
+    "",
+    [
+        "💲 Rent Prediction",
+        "📊 Interactive Graphs",
+        "📈 Map Visualisation"
+    ]
+)
+
+if page == "💲 Rent Prediction":
+    st.empty()
+    exec(open("rental.py").read())
+
+if page == "📊 Interactive Graphs":
+    st.empty()
+    exec(open("dashboard.py").read())
+
+if page == "📈 Map Visualisation":
+    st.empty()
+    exec(open("visual.py").read())
