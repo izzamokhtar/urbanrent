22413951ba2c1e2b7a7e7763d8bead4925a1908d
izzamokhtar <92832461+izzamokhtar@users.noreply.github.com>
2024-12-17 19:45:33 +0800

    Update rental.py

--- dashboard/rental.py
+++ dashboard/rental.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 from warnings import filterwarnings
 
 # Load the best model
-best_model_pipeline = joblib.load("model/best_model_pipeline.pkl")
-linear_model_pipeline = joblib.load('model/linear_regression_pipeline.pkl')
+best_model_pipeline = joblib.load(f"{getcwd()}/model/best_model_pipeline.pkl")
+linear_model_pipeline = joblib.load(f'{getcwd()}/model/linear_regression_pipeline.pkl')
 #random_forest_pipeline = joblib.load('/content/drive/MyDrive/FYP B IZZA/random_forest_pipeline.pkl')
-gradient_boosting_pipeline = joblib.load('model/gradient_boosting_pipeline.pkl')
+gradient_boosting_pipeline = joblib.load(f'{getcwd()}model/gradient_boosting_pipeline.pkl')
 
 # Load the best model's name
-with open("model/best_model_name.txt", "r") as f:
+with open(f"{getcwd()}/model/best_model_name.txt", "r") as f:
     best_model_name = f.read()
 
 # Extract categories for user input from the best model's preprocessor
